03e5ea010ddc8f53beff16c4a9c234b0eb8e5e10
Belgaux <oscarbelgau@gmail.com>
2017-09-28 13:15:29 +0200

    vector vector dot product with tests

--- src/main/scala/VecVecDot.scala
+++ src/main/scala/VecVecDot.scala
@@ -18,42 +18,36 @@ class VecVecDot() extends Module {
     bvd.io.a := io.a
     bvd.io.b := io.b
    
-    when(accumulated === UInt(0)){
+    when(io.write_enable){
       accumulator := accumulator + bvd.io.out
       accumulated := UInt(1)
-    }
-
-    when (io.write_enable) {
-        accumulated := UInt(0)
     }
 
     when (io.reset) {
       accumulator := UInt(0)  
     }
 
-    io.out := accumulator
     io.data_valid := accumulated
+    io.out := accumulator
 }
 
 
 class VecVecDotTest(c: VecVecDot) extends Tester(c) {
-  for(testNum <- 0 until 1){
+  for(testNum <- 0 until 10){
     poke(c.io.write_enable, 0)
     poke(c.io.reset, 1)
 
     step(1)
     poke(c.io.reset, 0)
 
-    val num_vec_parts = 2 //+ rnd.nextInt(3)
+    val num_vec_parts = 2 + rnd.nextInt(3)
 
     var expected_total_sum = 0
 
     for(_ <- 0 until num_vec_parts){
       
-      //val a = rnd.nextInt(Math.pow(2, 32).toInt)
-      //val b = rnd.nextInt(Math.pow(2, 32).toInt)
-      val a = 0xf
-      val b = 0xf
+      val a = rnd.nextInt(Math.pow(2, 32).toInt)
+      val b = rnd.nextInt(Math.pow(2, 32).toInt)
       val andy = a & b
 
       var expected_partial_sum = 0
@@ -68,12 +62,13 @@ class VecVecDotTest(c: VecVecDot) extends Tester(c) {
 
       poke(c.io.a, a)
       poke(c.io.b, b)
-
       poke(c.io.write_enable, 1)
 
+      step(1)
+
       while(peek(c.io.data_valid) != 1){
-       step(1)
-       poke(c.io.write_enable, 0)
+        poke(c.io.write_enable, 0)
+        step(1)
       }
       peek(c.io.out)
     }
