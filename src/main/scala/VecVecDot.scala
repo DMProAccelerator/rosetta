1532d9ce48e6e5975af2e5b1b8313ea72c47fdf6
Belgaux <oscarbelgau@gmail.com>
2017-10-08 14:14:23 +0200

    Now shit works perfectly!

--- src/main/scala/VecVecDot.scala
+++ src/main/scala/VecVecDot.scala
@@ -17,8 +17,6 @@ class VecVecDot(w: Int) extends Module {
   val s_idle :: s_running :: s_finished :: Nil = Enum(UInt(), 3)
   val state = Reg(init=UInt(s_idle))
   val acc = Reg(init=UInt(0, 32))
-  val a_tmp = Reg(init=UInt(0, 32))
-  val b_tmp = Reg(init=UInt(0, 32))
   val bytes_left = Reg(init=UInt(0, 32))
   val bytes_per_elem = w/8 // e.g: 32 / 8 -> 4
 
@@ -29,9 +27,6 @@ class VecVecDot(w: Int) extends Module {
   io.a.ready := Bool(false)
   io.b.ready := Bool(false)
   
-  a_tmp := io.a.bits
-  b_tmp := io.b.bits
-
   switch(state) {
     is(s_idle) {
       acc := UInt(0)
@@ -41,10 +36,10 @@ class VecVecDot(w: Int) extends Module {
     is(s_running) {
       when (bytes_left === UInt(0)) { state := s_finished }
       .otherwise {
-        io.a.ready := Bool(true)
-        io.b.ready := Bool(true)
         when (io.a.valid & io.b.valid) {
-          acc := acc + PopCount(a_tmp & b_tmp)
+          io.a.ready := Bool(true)
+          io.b.ready := Bool(true)
+          acc := acc + PopCount(io.a.bits & io.b.bits)
           bytes_left := bytes_left - UInt(bytes_per_elem)
         }
       }
